ce9340d3f340bbb2fa247592e96a906a8fb482ee
Charles Danielle Paglingayen <65948329+cdpaglingayen@users.noreply.github.com>
2022-10-28 22:14:56 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+const allButtons = document.querySelectorAll("button");
+for (let i = 0; i < allButtons.length; i++ ) {
+    allButtons[i].addEventListener("click", function () { 
+        
+        var buttonInnerHTML = this.innerHTML;
+
+        keyboardSound(buttonInnerHTML);
+
+        buttonAnimation(buttonInnerHTML);
+       
+    }) 
+
+}
+
+document.addEventListener("keydown", function(event) {
+    keyboardSound(event.key);
+    buttonAnimation(event.key);
+})
+
+function keyboardSound(key) {
+    switch (key) {
+        case "w":
+            var tomOne = new Audio('sounds/tom-1.mp3');
+            tomOne.play();
+            break;
+        case "a":
+            var tomTwo = new Audio('sounds/tom-2.mp3');
+            tomTwo.play();
+        break;
+        case "s":
+            var tomThree = new Audio('sounds/tom-3.mp3');
+            tomThree.play();
+        break;
+        case "d":
+            var tomFour = new Audio('sounds/tom-4.mp3');
+            tomFour.play();
+        break;
+        case "j":
+            var snare = new Audio('sounds/snare.mp3');
+            snare.play();
+        break;
+        case "k":
+            var crash = new Audio('sounds/crash.mp3');
+            crash.play();
+        break;
+        case "l":
+            var kick = new Audio('sounds/kick-bass.mp3');
+            kick.play();
+        break;
+        default:
+            console(this);
+    }
+}
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
